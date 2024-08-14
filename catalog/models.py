242ec33c2166199462b2f0fca00d293d5bcc4849
alina <khomkolovaalina@gmail.com>
2024-08-14 15:57:13 +0200

    добавила поле «Дата производства продукта» ( manufactured_at )

--- catalog/models.py
+++ catalog/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания (записи в БД)')
     updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения (записи в БД)')
+    manufactured_at = models.DateField(verbose_name='Дата производства', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}\n'
