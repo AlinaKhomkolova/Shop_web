65adf2db210b0789be4ba80cb1ceec11277b77c6
alina <khomkolovaalina@gmail.com>
2024-08-23 17:21:34 +0200

    Отформатировала по PEP

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import contacts, home
 
-
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', home, name='home'),
