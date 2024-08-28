ab9fa02f07316e9f938898e2044aae689d771b07
alina <khomkolovaalina@gmail.com>
2024-08-28 18:05:14 +0200

    Перевела имеющиеся контроллеры с FBV на CBV.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, menu, create_product, ProductListView, ProductDetailView
+from catalog.views import ProductListView, ProductDetailView, MenuListView, ContactsView, CreateProduct
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
-    path('contacts.html', contacts, name='contacts'),
-    path('menu.html', menu, name='menu'),
+    path('contacts.html', ContactsView.as_view(), name='contacts'),
+    path('menu.html', MenuListView.as_view(), name='menu'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_description'),
-    path('create_product.html', create_product, name='create_product'),
+    path('create_product.html', CreateProduct.as_view(), name='create_product'),
 ]
